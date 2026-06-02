8dbc859509fb221d88ed0a480f26ef27d3952c5d
Crissel <crissellitto7cc@gmail.com>
2026-06-02 21:03:25 +0530

    Update game.py
    improve game

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ secret = 7
 guess = int(input("Guess a number between 1 and 10: "))
 
 if guess == secret:
-    print("You win!")
+    print("🎉 You win!")
+elif guess < secret:
+    print("Too low!")
 else:
-    print("Wrong guess!")
+    print("Too high!")
